e36417ab641c160238fc01859c2855022b744282
firdanisa1415 <firdanisa1415@gmail.com>
2022-05-24 14:36:09 +0700

    chart

--- src/views/Dashboard/Chart.vue
+++ src/views/Dashboard/Chart.vue
@@ -1,5 +1,7 @@
 <script>
+import Card from "../../components/Cards/Card.vue";
 export default {
+  components: { Card },
   metaInfo: () => ({
     title: "Beranda",
   }),
@@ -10,9 +12,13 @@ export default {
 </script>
 
 <template>
-  <main-layout :title="pageTitle">
+  <main-layout :title="pageTitle" is-full>
     <template #body>
-      <p class="display-3 text-center">Halaman untuk menampilkan statistik chart</p>
+      <div class="col-12">
+        <base-input name="arietas" placeholder="Varietas" label="Komoditas">
+          <multi-select v-model="input.commodity" :options="g$ddCommodity" label="name" track-by="id" placeholder="Pilih Komoditas" :show-labels="false" />
+        </base-input>
+      </div>
     </template>
   </main-layout>
 </template>
