7fd9772bc5284feaa0b1880f26664abb1299879d
Khrisna Arief Wicaksono <khrisnaarief.2019@student.uny.ac.id>
2022-05-31 11:25:24 +0700

    fixing api detailKesehatan

--- src/services/masterData/kesehatan.js
+++ src/services/masterData/kesehatan.js
@@ -7,6 +7,6 @@ const list = () => baseApi.get(`${api}/${kesehatan}`);
 const add = (req) => baseApi.post(`${api}/${kesehatan}`, req);
 const edit = (req) => baseApi.put(`${api}/${kesehatan}/${req.id}`, req);
 const del = (req) => baseApi.delete(`${api}/${kesehatan}/${req}`);
-const detailKesehatan = (req) => baseApi.get(`${api}/${kesehatan}/list/${req.id}`, req);
+const detailKesehatan = (req) => baseApi.get(`${api}/${kesehatan}/list/${req}`);
 
 export { list, add, edit, del, detailKesehatan };
