e64108bc4169a2de8b9bb6053321ce58daad273a
abdiamn <abdiamn08@gmail.com>
2022-11-29 12:33:46 +0700

    perbaikan halaman verify

--- src/views/Auth/VerifyView.vue
+++ src/views/Auth/VerifyView.vue
@@ -1,29 +1,31 @@
 <template>
   <!-- Header -->
-  <div class="bg-gradient-argon py-7 py-lg-8 pt-lg-9">
+  <div class="mt-9">
     <div class="container">
-      <div class="header-body text-center mb-7">
-        <div class="row justify-content-center">
-          <div class="col-xl-5 col-lg-6 col-md-8 px-5">
-            <h1 class="text-secondary">Cek Email Kamu</h1>
-            <img
-              width="200"
-              style="margin: 20px 0 20px 60px"
-              src="../../../public/images/email.svg"
-              alt="email"
-            />
-            <p class="text-lead text-white">
-              Link verifikasi telah dikirim ke email kamu. Silahkan cek email
-              kamu untuk melakukan verifikasi.
-            </p>
+      <div class="row justify-content-center">
+        <div class="col-lg-6 col-md-8">
+          <div class="card bg-secondary shadow border-0">
+            <div class="card-header bg-transparent pb-5 text-center">
+              <h1 class="text-dark">Cek Email Kamu</h1>
+              <img
+                width="200"
+                style="margin: 20px 0 20px 60px"
+                src="../../../public/images/email.svg"
+                alt="email"
+              />
+              <p class="text-lead text-dark">
+                Link verifikasi telah dikirim ke email kamu. Silahkan cek email
+                kamu untuk melakukan verifikasi.
+              </p>
 
-            <router-link
-              to="/auth/login"
-              class="font-weight-bold text-white mt-5"
-              ><base-button type="primary"
-                >Kembali Ke Halaman Login</base-button
-              ></router-link
-            >
+              <router-link to="/auth/login"
+                ><base-button
+                  type="success1"
+                  class="my-2 btn-lg px-4 rounded-lg text-white"
+                  >Kembali Ke Halaman Login</base-button
+                ></router-link
+              >
+            </div>
           </div>
         </div>
       </div>
