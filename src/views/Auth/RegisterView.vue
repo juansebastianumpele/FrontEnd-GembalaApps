09ee148816544fe5a37747ec9d6f398f7b1c81b1
abdiamn <abdiamn08@gmail.com>
2022-11-30 13:53:09 +0700

    perbaikan page register

--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -60,108 +60,124 @@ export default {
 </script>
 
 <template>
-  <div class="header py-9 pt-md-9"></div>
+  <div class="header py-5 pt-sm-5"></div>
   <div class="container">
-    <div class="row mx-0 justify-content-center mt--9">
-      <div class="col-sm-2 col-sm-5 mt--6">
+    <div class="row mx-0 justify-content-center">
+      <div class="col-sm-8 mt--6">
+        <div class="text-center mt-2 mb-2">
+          <h1>Daftar Akun</h1>
+          <br /><br />
+          <img width="100" src="../../../public/images/logo.png" alt="email" />
+          <br />
+          <br />
+          <br />
+        </div>
         <div class="card bg-secondary shadow border-0">
           <div class="card-header bg-transparent pb-5">
-            <div class="text-center mt-2 mb-3">
-              <strong>Daftar Akun</strong>
-              <br />
-            </div>
             <form-comp :validation-schema="schema" @submit="onSubmit">
-              <base-input
-                name="nama_pengguna"
-                form-classes="input-group-alternative"
-                label="Nama Pengguna (Alfanumerik)"
-                placeholder="Nama Pengguna"
-                addon-left-icon="fas fa-user"
-                required
-              >
-              </base-input>
+              <div class="row">
+                <div class="col-sm-6">
+                  <base-input
+                    name="nama_pengguna"
+                    form-classes="input-group-alternative"
+                    label="Nama Pengguna (Alfanumerik)"
+                    placeholder="Nama Pengguna"
+                    addon-left-icon="fas fa-user"
+                    required
+                  >
+                  </base-input>
+                </div>
+                <div class="col-sm-6">
+                  <base-input
+                    name="email"
+                    form-classes="input-group-alternative"
+                    placeholder="Email"
+                    label="Email"
+                    addon-left-icon="fas fa-envelope"
+                    focused
+                    required
+                  >
+                  </base-input>
+                </div>
+                <div class="col-sm-6">
+                  <base-input
+                    name="nomor_telepon"
+                    form-classes="input-group-alternative"
+                    placeholder="Nomor Telepon"
+                    label="Nomor Telepon"
+                    addon-left-icon="fas fa-phone"
+                    focused
+                    required
+                  >
+                  </base-input>
+                </div>
+                <div class="col-sm-6">
+                  <base-input
+                    name="nama_peternakan"
+                    form-classes="input-group-alternative"
+                    label="Nama Peternakan"
+                    placeholder="Nama Peternakan"
+                    addon-left-icon="fas fa-house"
+                    required
+                  >
+                  </base-input>
+                </div>
+                <div class="col-sm-6">
+                  <base-input
+                    name="alamat"
+                    form-classes="input-group-alternative"
+                    placeholder="Alamat"
+                    label="Alamat"
+                    addon-left-icon="fas fa-location-arrow"
+                    focused
+                    required
+                  >
+                  </base-input>
+                </div>
+                <div class="col-sm-6">
+                  <base-input
+                    name="postcode"
+                    form-classes="input-group-alternative"
+                    placeholder="Kode Pos"
+                    label="Kode Pos"
+                    addon-left-icon="fas fa-signs-post"
+                    focused
+                    required
+                  >
+                  </base-input>
+                </div>
+                <div class="col-sm-6">
+                  <base-input
+                    name="kata_sandi"
+                    form-classes="input-group-alternative"
+                    placeholder="Kata Sandi"
+                    label="Kata Sandi"
+                    type="password"
+                    addon-left-icon="fas fa-lock"
+                    password
+                    required
+                  >
+                  </base-input>
+                </div>
+                <div class="col-sm-6">
+                  <base-input
+                    name="ulangi_kata_sandi"
+                    form-classes="input-group-alternative"
+                    placeholder="Ulangi Kata Sandi"
+                    label="Ulangi Kata Sandi"
+                    type="password"
+                    addon-left-icon="fas fa-lock"
+                    password
+                    required
+                  >
+                  </base-input>
+                </div>
+              </div>
 
-              <base-input
-                name="email"
-                form-classes="input-group-alternative"
-                placeholder="Email"
-                label="Email"
-                addon-left-icon="fas fa-envelope"
-                focused
-                required
-              >
-              </base-input>
-
-              <base-input
-                name="nomor_telepon"
-                form-classes="input-group-alternative"
-                placeholder="Nomor Telepon"
-                label="Nomor Telepon"
-                addon-left-icon="fas fa-phone"
-                focused
-                required
-              >
-              </base-input>
-
-              <base-input
-                name="nama_peternakan"
-                form-classes="input-group-alternative"
-                label="Nama Peternakan"
-                placeholder="Nama Peternakan"
-                addon-left-icon="fas fa-house"
-                required
-              >
-              </base-input>
-
-              <base-input
-                name="alamat"
-                form-classes="input-group-alternative"
-                placeholder="Alamat"
-                label="Alamat"
-                addon-left-icon="fas fa-location-arrow"
-                focused
-                required
-              >
-              </base-input>
-
-              <base-input
-                name="postcode"
-                form-classes="input-group-alternative"
-                placeholder="Kode Pos"
-                label="Kode Pos"
-                addon-left-icon="fas fa-signs-post"
-                focused
-                required
-              >
-              </base-input>
-
-              <base-input
-                name="kata_sandi"
-                form-classes="input-group-alternative"
-                placeholder="Kata Sandi"
-                label="Kata Sandi"
-                type="password"
-                addon-left-icon="fas fa-lock"
-                password
-                required
-              >
-              </base-input>
-
-              <base-input
-                name="ulangi_kata_sandi"
-                form-classes="input-group-alternative"
-                placeholder="Ulangi Kata Sandi"
-                label="Ulangi Kata Sandi"
-                type="password"
-                addon-left-icon="fas fa-lock"
-                password
-                required
-              >
-              </base-input>
               <div class="text-center">
                 <base-button
                   type="success1"
-                  class="my-2 btn-lg px-4 rounded-lg text-white"
+                  class="my-2 btn-lg px-6 rounded-lg text-white"
                   native-type="submit"
                 >
                   Buat Akun
