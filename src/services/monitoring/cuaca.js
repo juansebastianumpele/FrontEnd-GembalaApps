d337f1d7b2d1de5b411833c87ac59b4d67b2c2fb
muhammadhanif010 <muhammad.hanif010@binus.ac.id>
2022-12-04 16:16:31 +0700

    axios await

--- src/services/monitoring/cuaca.js
+++ src/services/monitoring/cuaca.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const getWeather = (latitude, longitude, appid) => axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=id&appid=${appid}`);
-const getWeatherPrediction = (latitude, longitude, appid) => axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&units=metric&lang=id&cnt=1&appid=${appid}`);
+const getWeather = async (latitude, longitude, appid) => await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=id&appid=${appid}`);
+const getWeatherPrediction = async (latitude, longitude, appid) => await axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&units=metric&lang=id&cnt=1&appid=${appid}`);
 
 export {
     getWeather,
