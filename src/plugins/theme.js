34f3d378ee3480c5a48c32db69d3fb010f5c92df
muhammadhanif010 <muhammad.hanif010@binus.ac.id>
2022-12-16 09:52:40 +0700

    Hapus fontawesome dari theme.js
    Dihapus agar hanya mengambil font awesome dari cdn dan bukan dari server agar tidak error failed to decode font and OTS parsing error

--- src/plugins/theme.js
+++ src/plugins/theme.js
@@ -7,7 +7,6 @@ import HighchartsVue from "highcharts-vue";
 
 import "@/assets/sass/argon.scss";
 
-import "@/assets/vendor/fontawesome-free/css/all.min.css";
 import "@/assets/vendor/multiselect/vue-multiselect.min.css";
 import "@/assets/vendor/flatpickr/style.css";
 import "@/assets/vendor/flatpickr/monthStyle.css";
